75776642e1031892683c61f3a71416b82cb7fbb4
David-Vermaak <kaamrev1@gmail.com>
2025-04-16 11:22:39 -0400

    Add project files.

---
+++ InventorAutomationProject/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace InventorAutomationProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.shapesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sphereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gearsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spurGearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helicalGearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bevelGearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wormGearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLength
+            // 
+            this.txtLength.CausesValidation = false;
+            this.txtLength.Location = new System.Drawing.Point(151, 73);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Width";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(151, 108);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(151, 147);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Create a Cube!";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(101, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(368, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shapesToolStripMenuItem,
+            this.gearsToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(51, 22);
+            this.toolStripDropDownButton1.Text = "Select";
+            // 
+            // shapesToolStripMenuItem
+            // 
+            this.shapesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cubeToolStripMenuItem,
+            this.sphereToolStripMenuItem});
+            this.shapesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("shapesToolStripMenuItem.Image")));
+            this.shapesToolStripMenuItem.Name = "shapesToolStripMenuItem";
+            this.shapesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shapesToolStripMenuItem.Text = "Shapes";
+            // 
+            // cubeToolStripMenuItem
+            // 
+            this.cubeToolStripMenuItem.Name = "cubeToolStripMenuItem";
+            this.cubeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cubeToolStripMenuItem.Text = "Cube";
+            // 
+            // sphereToolStripMenuItem
+            // 
+            this.sphereToolStripMenuItem.Name = "sphereToolStripMenuItem";
+            this.sphereToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sphereToolStripMenuItem.Text = "Sphere";
+            this.sphereToolStripMenuItem.Click += new System.EventHandler(this.sphereToolStripMenuItem_Click);
+            // 
+            // gearsToolStripMenuItem
+            // 
+            this.gearsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spurGearToolStripMenuItem,
+            this.helicalGearToolStripMenuItem,
+            this.bevelGearToolStripMenuItem,
+            this.wormGearToolStripMenuItem});
+            this.gearsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("gearsToolStripMenuItem.Image")));
+            this.gearsToolStripMenuItem.Name = "gearsToolStripMenuItem";
+            this.gearsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gearsToolStripMenuItem.Text = "Gears";
+            // 
+            // spurGearToolStripMenuItem
+            // 
+            this.spurGearToolStripMenuItem.Name = "spurGearToolStripMenuItem";
+            this.spurGearToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.spurGearToolStripMenuItem.Text = "Spur Gear";
+            // 
+            // helicalGearToolStripMenuItem
+            // 
+            this.helicalGearToolStripMenuItem.Name = "helicalGearToolStripMenuItem";
+            this.helicalGearToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.helicalGearToolStripMenuItem.Text = "Helical Gear";
+            // 
+            // bevelGearToolStripMenuItem
+            // 
+            this.bevelGearToolStripMenuItem.Name = "bevelGearToolStripMenuItem";
+            this.bevelGearToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.bevelGearToolStripMenuItem.Text = "Bevel Gear";
+            // 
+            // wormGearToolStripMenuItem
+            // 
+            this.wormGearToolStripMenuItem.Name = "wormGearToolStripMenuItem";
+            this.wormGearToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.wormGearToolStripMenuItem.Text = "Worm Gear";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(368, 300);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLength);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem shapesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sphereToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gearsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spurGearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helicalGearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bevelGearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wormGearToolStripMenuItem;
+    }
+}
+
---
+++ InventorAutomationProject/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+//Imports System.Type
+//Imports System.Activator
+using System.Runtime.InteropServices;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+using System.Windows.Forms;
+using DarkModeForms;
+using Inventor;
+
+namespace InventorAutomationProject
+{
+    public partial class Form1 : Form
+    {
+        Inventor.Application _invApp;
+        bool _started = false;
+
+        private DarkModeCS dm;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dm = new DarkModeCS(this)
+            {
+                //[Optional] Choose your preferred color mode here:
+                ColorMode = DarkModeCS.DisplayMode.SystemDefault
+            };
+
+            try
+            {
+                _invApp = (Inventor.Application)Marshal.GetActiveObject("Inventor.Application");
+
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    Type invAppType = Type.GetTypeFromProgID("Inventor.Application");
+
+                    _invApp = (Inventor.Application)System.Activator.CreateInstance(invAppType);
+                    _invApp.Visible = true;
+
+                    //Note: if the Inventor session is left running after this
+                    //form is closed, there will still be an Inventor.exe 
+                    //running. We will use this Boolean to test in Form1.Designer.cs 
+                    //in the dispose method whether or not the Inventor App should
+                    //be shut down when the form is closed.
+                    _started = true;
+
+                }
+                catch (Exception ex2)
+                {
+                    MessageBox.Show(ex2.ToString());
+                    MessageBox.Show("Unable to get or start Inventor");
+                }
+            }
+        }
+
+        private void CreateCube(double height, double width, double length)
+        {
+            // Create a new part document
+            PartDocument partDoc = (PartDocument)_invApp.Documents.Add(DocumentTypeEnum.kPartDocumentObject,
+                                                                            _invApp.FileManager.GetTemplateFile(DocumentTypeEnum.kPartDocumentObject),
+                                                                            true);
+
+            PartComponentDefinition compDef = partDoc.ComponentDefinition;
+            PlanarSketch sketch = compDef.Sketches.Add(compDef.WorkPlanes[3]); // XY Plane
+
+            // Draw rectangle on sketch
+            TransientGeometry tg = _invApp.TransientGeometry;
+            sketch.SketchLines.AddAsTwoPointRectangle(
+                tg.CreatePoint2d(0, 0),
+                tg.CreatePoint2d(width, length)
+            );
+
+            // Profile and extrude
+            Profile profile = sketch.Profiles.AddForSolid();
+            ExtrudeDefinition extDef = compDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(
+                profile, PartFeatureOperationEnum.kJoinOperation);
+            extDef.SetDistanceExtent(height, PartFeatureExtentDirectionEnum.kPositiveExtentDirection);
+
+            compDef.Features.ExtrudeFeatures.Add(extDef);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double height = Convert.ToDouble(txtHeight.Text);
+                double width = Convert.ToDouble(txtWidth.Text);
+                double length = Convert.ToDouble(txtLength.Text);
+
+                CreateCube(height, width, length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void sphereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
